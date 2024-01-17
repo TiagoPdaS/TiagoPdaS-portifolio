558ee4e86031bd63f1db09d444fe6b26f10b6416
Tiago Da Silva <tiagopdasgithub@gmail.com>
2024-01-16 22:20:22 -0500

    3.0

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+/*============TOGGLE ICON NAVBAR=============== */
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bi-x-lg');
+    navbar.classList.toggle('active');
+}
+
+
+/*============SCROLL SECTIONS ACTIVE LINK==============*/
 let sections = document.querySelectorAll('section')
 let navLinks = document.querySelectorAll('header navbar a')
 
@@ -18,5 +29,13 @@ window.onscroll = () => {
         };
     
     });
+    /* =========sticky navbar */
+    let header = document.querySelector('header')
+    header.classList.toggle('sticky', window.scrollY > 100)
+
+    /*===========REMOVE TOGGLE ICON AND NAVBAR WHEN CLICK NAVBAR LINK(SCROLL)*/
+    menuIcon.classList.remove('bi-x-lg');
+    navbar.classList.remove('active');
 };
 
+s
