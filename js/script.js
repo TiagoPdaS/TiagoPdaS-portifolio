799b974bafb2680e5325a763466ca7cc3195e5ca
Tiago Da Silva <tiagopdasgithub@gmail.com>
2024-01-22 22:36:45 -0500

    4.0

--- js/script.js
+++ js/script.js
@@ -38,3 +38,24 @@ window.onscroll = () => {
     navbar.classList.remove('active');
 };
 
+/* =============scroll reveal======================*/
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 3000,
+    delay:300
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin:'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin:'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img, .techs-container ', { origin:'left' });
+ScrollReveal().reveal('.home-content p, .about-content, .clock', { origin:'right' });
+
+/* =============typed js======================*/
+const typed = new Typed('.multiple-text',{
+    strings: ['FrontEnd', 'Developer', 'FRONTEND DEVELOPER'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
