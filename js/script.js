87d7944932d35b27cf2d561d8d10272f94d9cefd
Tiago Da Silva <tiagopdasgithub@gmail.com>
2024-01-23 22:33:33 -0500

    5.0

--- js/script.js
+++ js/script.js
@@ -59,3 +59,31 @@ const typed = new Typed('.multiple-text',{
     backDelay:1000,
     loop:true
 })
+
+/* ===============form buttom send message========== */
+/*
+$(document).ready(function() {
+    $('#formulario').submit(function(e) {
+      e.preventDefault();
+      var nome = $('#nome').val();
+      var email = $('#email').val();
+      var mensagem = $('#mensagem').val();
+
+      $.ajax({
+        type: 'POST',
+        url: 'enviar_email.php',
+        data: {
+          nome: nome,
+          email: email,
+          mensagem: mensagem
+        },
+        success: function(response) {
+          alert('Mensagem enviada com sucesso!');
+          $('#formulario')[0].reset();
+        },
+        error: function() {
+          alert('Ocorreu um erro ao enviar a mensagem. Por favor, tente novamente.');
+        }
+      });
+    });
+  });*/
