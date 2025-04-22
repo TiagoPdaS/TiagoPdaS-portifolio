ec1f9f9887c1c9ffff97255098f5843ac682fca1
myuser <myemail>
2025-04-21 21:57:45 -0400

    estilos
    melhorando visual

--- js/clock.js
+++ js/clock.js
@@ -9,13 +9,10 @@ const relogio = setInterval(function time() {
     let second = dateToday.getSeconds();
 
     if (hour < 10) hour = '0' + hour;
-
     if (minute < 10) minute = '0' + minute;
-
     if (second < 10) second = '0' + second;
 
     hours.textContent = hour;
     minutes.textContent = minute;
     seconds.textContent = second;
-
-})
+});
--- js/script.js
+++ js/script.js
@@ -7,31 +7,28 @@ menuIcon.onclick = () => {
     navbar.classList.toggle('active');
 }
 
-
 /*============SCROLL SECTIONS ACTIVE LINK==============*/
-let sections = document.querySelectorAll('section')
-let navLinks = document.querySelectorAll('header navbar a')
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a'); // Corrigido seletor
 
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
-        let offset = sec.offsetTop -150;
+        let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
     
-        if(top >= offset && top < offset + height) {
+        if (top >= offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector
-                ('header navbar a [href*=' + id + ']').classList.add
-                ('active');
+                document.querySelector(`header .navbar a[href*=${id}]`).classList.add('active'); // Corrigido seletor
             });
-        };
-    
+        }
     });
+
     /* =========sticky navbar */
-    let header = document.querySelector('header')
-    header.classList.toggle('sticky', window.scrollY > 100)
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
 
     /*===========REMOVE TOGGLE ICON AND NAVBAR WHEN CLICK NAVBAR LINK(SCROLL)*/
     menuIcon.classList.remove('bi-x-lg');
@@ -43,22 +40,21 @@ ScrollReveal({
     reset: true,
     distance: '80px',
     duration: 2000,
-    delay:50
+    delay: 50
 });
 
-ScrollReveal().reveal('.home-content, .heading', { origin:'top' });
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin:'bottom' });
-ScrollReveal().reveal('.home-content h1, .about-img, .techs-container ', { origin:'left' });
-ScrollReveal().reveal('.home-content p, .about-content, .about-container h3, .about-container p, .clock', { origin:'right' });
-
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img, .techs-container', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content, .about-container h3, .about-container p, .clock', { origin: 'right' });
 
 /* =============typed js======================*/
-const typed = new Typed('.multiple-text',{
+const typed = new Typed('.multiple-text', {
     strings: ['Front-End', 'Developer', 'Front-End Developer'],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
-    loop:true
-})
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
 
 
